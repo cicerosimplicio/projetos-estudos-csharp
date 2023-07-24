401e3479ef378f44167b95c1968a336052ba837e
Cicero Simplicio <79380436+cicerosimplicio@users.noreply.github.com>
2023-07-24 08:51:18 -0300

    teste de conflito

--- 01-worker-app/WorkerApp/Entities/Department.cs
+++ 01-worker-app/WorkerApp/Entities/Department.cs
@@ -1,17 +1,19 @@
 ﻿namespace WorkerApp.Entities
 {
-    internal class Department
+    internal class Department //teste de conflito
     {
         public string Name { get; set; }
 
         public Department()
         {
-
+            //teste de conflito
         }
 
         public Department(string name)
         {
             Name = name;
         }
+
+        //teste de conflito
     }
 }

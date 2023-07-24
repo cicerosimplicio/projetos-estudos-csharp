e6b589f4de18338bb9d35457f53aa7238323c033
Cicero Simplicio <ciceroandrade95@gmail.com>
2023-07-24 08:54:34 -0300

    teste de conflito com branch local para fix ou feature

--- 01-worker-app/WorkerApp/Entities/Department.cs
+++ 01-worker-app/WorkerApp/Entities/Department.cs
@@ -1,17 +1,19 @@
 ﻿namespace WorkerApp.Entities
 {
-    internal class Department
+    internal class Department //aqui foi onde forcei o conflito
     {
         public string Name { get; set; }
 
         public Department()
         {
-
+            //aqui foi onde forcei o conflito
         }
 
         public Department(string name)
         {
             Name = name;
         }
+
+        //aqui foi onde forcei o conflito
     }
 }

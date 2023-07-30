d3e98c13e317b8b9bc3aa29befd378811cb9c972
Cicero Simplicio <ciceroandrade95@gmail.com>
2023-07-30 07:58:13 -0300

    adicionado a classe Worker

---
+++ 01-worker-app/WorkerApp/Entities/Worker.cs
@@ -0,0 +1,51 @@
+﻿using WorkerApp.Entities.Enums;
+
+namespace WorkerApp.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+        pu
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = departament;
+        }
+
+        public void AddContract(Contracts contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(Contracts contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach (Contracts contract in Contracts)
+            {
+                if (year == contract.Year && month == contract.Month)
+                {
+                    sum += contract.TotalValue
+                }
+            }
+           
+            return sum;
+        }
+    }
+}

66813f705f1ae18621608d21e11e248659be0f7b
Cicero Simplicio <ciceroandrade95@gmail.com>
2023-08-09 10:53:56 -0300

    Adicionado a classe OrderItem

---
+++ 02-store-app/StoreApp/Entities/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿namespace StoreApp.Entities
+{
+    internal class OrderItem
+    {
+        public Product Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(Product name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+    }
+}

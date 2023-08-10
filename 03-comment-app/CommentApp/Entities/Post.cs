0aca0827476f6f7da3b0394cd7a386b2b30b3f04
Cicero Simplicio <ciceroandrade95@gmail.com>
2023-08-10 11:34:08 -0300

    Adicionado a classe Post

---
+++ 03-comment-app/CommentApp/Entities/Post.cs
@@ -0,0 +1,33 @@
+﻿namespace CommentApp.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment text)
+        {
+            Comments.Add(text);
+        }
+
+        public void RemoveComment(Comment text)
+        {
+            Comments.Remove(text);
+        }
+    }
+}

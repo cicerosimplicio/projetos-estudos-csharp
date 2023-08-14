1754b3ec8bbb85643eaf585e57d6865b4f4a290d
Cicero Simplicio <ciceroandrade95@gmail.com>
2023-08-14 10:39:38 -0300

    Adicionado a classe Employee

---
+++ 05-employee-app/EmployeeApp/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace EmployeeApp.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public double Payment()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+}

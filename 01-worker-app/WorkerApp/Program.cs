3101573f6f55c7d68f2f68798ce6d87f74022126
Cicero Simplicio <ciceroandrade95@gmail.com>
2023-09-09 11:33:43 -0300

    Update Program.cs

--- 01-worker-app/WorkerApp/Program.cs
+++ 01-worker-app/WorkerApp/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace WorkerApp
+﻿using WorkerApp.Entities;
+using WorkerApp.Entities.Enums;
+
+namespace WorkerApp
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {         
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/Pleno/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary: ");
+            double baseSalary = int.Parse(Console.ReadLine());
+
+            Department department = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, department);
+
+            Console.Write("How many contracts to this worker? ");
+            int totalContracts = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= totalContracts; i++)
+            {
+                Console.WriteLine($"Enter {i}# contract data:");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+            }
         }
     }
 }

dc62b4435b4f03290c68467da617487163357d28
Cicero Simplicio <ciceroandrade95@gmail.com>
2023-09-13 17:49:57 -0300

    Updated

--- 02-store-app/StoreApp/Program.cs
+++ 02-store-app/StoreApp/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace StoreApp
+﻿using StoreApp.Entities;
+using StoreApp.Enums;
+
+namespace StoreApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int qtItems = int.Parse(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            for (int i = 1; i < qtItems; i++)
+            {
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName);
+                OrderItem item = new OrderItem(product, price, quantity);
+                order.AddItem(item);
+            }
         }
     }
 }

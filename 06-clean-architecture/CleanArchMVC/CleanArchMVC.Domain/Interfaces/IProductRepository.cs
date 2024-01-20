a585284f3d479d4d0714d8f2b1f327d286e340bb
CiceroSimplicio <ciceroandrade95@gmail.com>
2024-01-20 16:32:25 -0300

    Criação das interfaces

---
+++ 06-clean-architecture/CleanArchMVC/CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMVC.Domain.Entities;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> UpdateProductAsync(Product product);
+        Task<Product> DeleteProductAsync(Product product);
+    }
+}

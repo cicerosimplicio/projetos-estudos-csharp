a0f18f61aa34aca2d9e2c98e5bff36d3fc35ceb4
CiceroSimplicio <ciceroandrade95@gmail.com>
2024-03-26 17:32:36 -0300

    Camada Domain

---
+++ 06-clean-architecture/CleanArchMvc/CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int id);
+        Task<Category> CreateCategoryAsync(Category category);
+        Task<Category> UpdateCategoryAsync(Category category);
+        Task<Category> RemoveCategoryAsync(Category category);
+    }
+}
